048fb2763b62e118cd71b0feba0662e076f59665
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-25 17:11:41 +0530

    updated

---
+++ data.tf
@@ -0,0 +1,5 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "devops-ansible"
+  owners           = ["self"]
+}
---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {}
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+
