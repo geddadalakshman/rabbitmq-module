9f23ae135ff2ede8fa70ca9c8c001ce4d5de3071
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-05-04 12:25:53 +0530

    updated

--- data.tf
+++ data.tf
@@ -1,6 +1,13 @@
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "devops-ansible"
-  owners           = ["self"]
+  most_recent = true
+  name_regex  = "devops-practice-with-ansible"
+  owners      = ["self"]
 }
+
+data "aws_route53_zone" "domain" {
+  name = var.dns_domain
+}
+
+data "aws_caller_identity" "account" {}
+
 
--- vars.tf
+++ vars.tf
@@ -1,7 +1,11 @@
-variable "instance_type" {}
 variable "env" {}
 variable "tags" {}
 variable "subnet_ids" {}
-
-
-
+variable "instance_type" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
+variable "dns_domain" {}
+variable "component" {
+  default = "rabbitmq"
+}
